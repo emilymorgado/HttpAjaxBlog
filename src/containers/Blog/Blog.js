3d90ac9a673180279f4e8632f429f3a96687f236
Emily Morgado <emorgado@sfomlp-1jchv2h.ad.businesswire.com>
2018-06-11 10:11:59 -0700

    Set up routing and restructure files/folders

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,65 +2,30 @@ import React, { Component } from 'react';
 import axios from 'axios';
 // import axios from '../../axios';  This imports the axios instance
 
-import Post from '../../components/Post/Post';
-import FullPost from '../../components/FullPost/FullPost';
-import NewPost from '../../components/NewPost/NewPost';
+import Posts from './Posts/Posts';
 import './Blog.css';
 
 class Blog extends Component {
-    state = {
-      posts: [],
-      selectedPostId: null,
-      error: false,
-    }
-    componentDidMount () {
-      axios.get('https://jsonplaceholder.typicode.com/posts')
-        .then(response => {
-          const posts = response.data.slice(0, 4);
-          const updatedPosts = posts.map(post => {
-            return {
-              ...post,
-              author: 'Max'
-            }
-          })
-          this.setState({ posts: updatedPosts });
-          // was this:
-          // this.setState({ posts: response.data });
-        })
-        .catch(error => {
-            // console.log(error);
-            this.setState({error: true});
-        });
-    }
-
-    postSelectedHandler = (id) => {
-      this.setState({selectedPostId: id});
-    }
-    render () {
-        let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
-        // const posts = this.state.posts.map(post => {
-        if (!this.state.error) {
-          posts = this.state.posts.map(post => {
-          return <Post
-                  key={post.id}
-                  title={post.title}
-                  author={post.author}
-                  clicked={() => this.postSelectedHandler(post.id)} />;
-            });
-          }
-        return (
-            <div>
-                <section className="Posts">
-                    { posts }
-                </section>
-                <section>
-                    <FullPost id={this.state.selectedPostId}/>
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
+  render () {
+    return (
+      <div className='Blog'>
+        <header>
+          <nav>
+            <ul>
+              <li><a href='/'>Home</a></li>
+              <li><a href='/new-post'>New Post</a></li>
+            </ul>
+          </nav>
+        </header>
+        <Posts />
+          {/* <section>
+              <FullPost id={this.state.selectedPostId}/>
+          </section>
+          <section>
+              <NewPost />
+          </section> */}
+      </div>
+      );
     }
 }
 
